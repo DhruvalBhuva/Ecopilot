04a9b52f59f671bf1e6a510fb5a9de5df7b772c9
Dhruval Bhuva <dhruvalbhuva2000@gmail.com>
2025-03-04 17:48:55 +0100

    Add setup

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 from typing import List
 from setuptools import setup, find_packages
 
-HYPEN_E_DOT = '-e.'
+HYPEN_E_DOT = "-e ."
 
 
 def read_requirements(file_path: str) -> List[str]:
-    '''
-    this function will return the list of requirements
-    '''
-
+    """
+    This function will return the list of requirements.
+    """
     requirements = []
-    with open(file_path) as file_obj:
+    with open(file_path, encoding="utf-8") as file_obj:  # Ensure UTF-8 encoding
         requirements = file_obj.readlines()
-        requirements = [req.replace("\n", "") for req in requirements]
+        requirements = [
+            req.strip() for req in requirements if req.strip()
+        ]  # Remove empty lines and strip whitespace
 
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
@@ -21,7 +22,7 @@ def read_requirements(file_path: str) -> List[str]:
 
 
 setup(
-    name="",
+    name="ecopilot",
     version="0.0.1",
     author="Dhruval Bhuva",
     author_email="dhruvalbhuva2000@gmail.com",
